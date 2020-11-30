431ac622fbf535c3ed0969321f0f45410ca7d150
KieranLienau <kieran.lienau@draeger.com>
2020-11-30 20:03:40 +0100

    initiated the project

---
+++ xlsx_reader/xlsx_reader/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  xlsx_reader
+//
+//  Created by Kieran Lienau on 30.11.20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
